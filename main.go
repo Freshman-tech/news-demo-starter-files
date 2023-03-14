20021794703c08f5abf24797158ad9e9c65d880d
I535105 <manu.porwal@sap.com>
2023-03-15 01:01:48 +0530

    built hello world app

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
+package main
 
+import (
+	"net/http"
+	"os"
+)
+
+func indexhandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello World</h1"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexhandler)
+	http.ListenAndServe(":"+port, mux)
+}
