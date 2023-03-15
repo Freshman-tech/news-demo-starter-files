030dceaf4a65035240e2dd102c4ee6828237af1f
I535105 <manu.porwal@sap.com>
2023-03-15 11:00:13 +0530

    used godotenv package and extracted env variable from .env file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/freshman-tech/news-demo-starter-files
 
 go 1.15
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func indexhandler(w http.ResponseWriter, r *http.Request) {
@@ -10,10 +13,15 @@ func indexhandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexhandler)
 	http.ListenAndServe(":"+port, mux)
